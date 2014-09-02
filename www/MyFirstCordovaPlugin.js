ddbcecbd7d32ee75c49709a2f89533d55209d243
rpngithub <pramod.ravindran.nair@gmail.com>
2014-09-02 19:46:24 +0530

    Update MyFirstCordovaPlugin.js

--- www/MyFirstCordovaPlugin.js
+++ www/MyFirstCordovaPlugin.js
@@ -1,6 +1,5 @@
 // JavaScript Document
 var exec = require('cordova/exec');
-var platform = require('cordova/platform');
 
 var HelloPlugin = {
 	callNativeFunction : function(param, successCallback, errorCallback){
@@ -8,14 +7,4 @@ var HelloPlugin = {
 	}
 }
 
-function callNativePlugin( returnSuccess ) { 
-	HelloPlugin.callNativeFunction(returnSuccess, nativePluginResultHandler, nativePluginErrorHandler ); 
-} 
-function nativePluginResultHandler (result) { 
- alert("SUCCESS: \r\n"+result ); 
-} 
-function nativePluginErrorHandler (error) {  
-   alert("ERROR: \r\n"+error );
-}
-
-module.exports =  new HelloPlugin();
+module.exports =  HelloPlugin;
